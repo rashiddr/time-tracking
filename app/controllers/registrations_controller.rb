3a6ff56ba91bb8b998a637385cadb7c71aac7d1e
Rashid <rashid@qburst.com>
2016-09-21 14:56:04 +0530

    after sign up path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,4 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
   def after_inactive_sign_up_path_for(resource)
      new_user_session_path
   end
+  def after_sign_up_path_for(resource)
+  	User.profile_completed(current_user)
+  	root_path
+  end
 end
